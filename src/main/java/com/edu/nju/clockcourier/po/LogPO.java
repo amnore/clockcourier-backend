a3024c8609ac702d56e17b062c5f3cc3f10ba3e0
191250045-Hou Weidong <191250045@smail.nju.edu.cn>
2022-02-24 01:42:08 +0800

    feat: 修改 log 配置: startTime 改为 String

--- src/main/java/com/edu/nju/clockcourier/po/LogPO.java
+++ src/main/java/com/edu/nju/clockcourier/po/LogPO.java
@@ -4,37 +4,28 @@ import lombok.Data;
 
 @Data
 public class LogPO {
+
     // 操作描述
     private String description;
-
     // 操作用户
     private String username;
-
     //操作时间
-    private Long startTime;
-
+    private String startTime;
     // 消耗时间
     private Integer spendTime;
-
-    //根路径
+    // 根路径
     private String basePath;
-
     // URI
     private String uri;
-
-    //URL
+    // URL
     private String url;
-
     // 请求类型
     private String method;
-
     // IP地址
     private String ip;
-
     // 请求参数
     private Object parameter;
-
-    //  请求返回的结果
+    // 请求返回的结果
     private Object result;
 
 }
