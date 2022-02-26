1d8b4061dd2ba0608a2acd422553392dcab3fb65
191250045-Hou Weidong <191250045@smail.nju.edu.cn>
2022-02-26 21:44:37 +0800

    fix: 修复筛选规则为 “” 时异常的 bug

--- src/main/java/com/edu/nju/clockcourier/util/QueryLikeBuilder.java
+++ src/main/java/com/edu/nju/clockcourier/util/QueryLikeBuilder.java
@@ -3,7 +3,7 @@ package com.edu.nju.clockcourier.util;
 public class QueryLikeBuilder {
 
     public static String buildLike(String str) {
-        if (str == null) return null;
+        if (str == null || str.equals("")) return null;
         return "%" + str + "%";
     }
 
