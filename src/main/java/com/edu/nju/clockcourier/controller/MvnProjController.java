9fb24b5c11376b43aad3acb1a2f8382a3198c295
zzh <2830793096@qq.com>
2022-03-12 17:17:36 +0800

    feat:搭完mvn框架

--- src/main/java/com/edu/nju/clockcourier/controller/MvnProjController.java
+++ src/main/java/com/edu/nju/clockcourier/controller/MvnProjController.java
@@ -1,12 +1,13 @@
 package com.edu.nju.clockcourier.controller;
 
+import com.edu.nju.clockcourier.dto.MvnProjFilterDTO;
 import com.edu.nju.clockcourier.service.MvnProjService;
+import com.edu.nju.clockcourier.vo.MvnProjGraphVO;
 import com.edu.nju.clockcourier.vo.MvnProjVO;
+import com.edu.nju.clockcourier.vo.MvnProjVersionVO;
 import com.edu.nju.clockcourier.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/mvn")
@@ -19,9 +20,20 @@ public class MvnProjController {
         this.mvnProjService=mvnProjService;
     }
 
-    @GetMapping("/query")
-    public ResponseVO<MvnProjVO> get(){
+    @PostMapping("/query")
+    public ResponseVO<MvnProjVO> query(@RequestBody MvnProjFilterDTO filter){
         return ResponseVO.success(null);
     }
-    
+
+    @GetMapping("/{projectId}/{version}/mvn")
+    public ResponseVO<MvnProjVersionVO> getMvn(@PathVariable Integer projectId,@PathVariable String version){
+        return ResponseVO.success(null);
+    }
+
+    @GetMapping("/{projectId}/graph")
+    public ResponseVO<MvnProjGraphVO> getGraph(@PathVariable Integer projectId){
+        return ResponseVO.success(null);
+    }
+
+
 }
