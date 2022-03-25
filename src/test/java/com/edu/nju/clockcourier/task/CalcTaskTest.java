62e675a69f6e9a4d4ad3377309f942999fad3f54
191250045-Hou Weidong <191250045@smail.nju.edu.cn>
2022-03-26 04:18:56 +0800

    fix: 修复了使用 dev 构建失败的问题

--- src/test/java/com/edu/nju/clockcourier/task/CalcTaskTest.java
+++ src/test/java/com/edu/nju/clockcourier/task/CalcTaskTest.java
@@ -3,10 +3,8 @@ package com.edu.nju.clockcourier.task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 public class CalcTaskTest {
 
     private final CalcTask task;
