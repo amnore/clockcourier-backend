daf322a45fd2714abad0939753be55e818a15948
Chen Zhenge <me@markle.one>
2022-03-28 10:02:05 +0800

    api: expose migration calc

--- src/main/java/com/edu/nju/clockcourier/controller/MigrationController.java
+++ src/main/java/com/edu/nju/clockcourier/controller/MigrationController.java
@@ -1,11 +1,13 @@
 package com.edu.nju.clockcourier.controller;
 
 import com.edu.nju.clockcourier.service.MigrationService;
+import com.edu.nju.clockcourier.task.CalcTask;
 import com.edu.nju.clockcourier.vo.MigrationGraphVO;
 import com.edu.nju.clockcourier.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +17,20 @@ import java.util.List;
 @RequestMapping("/migration")
 public class MigrationController {
 
-    private final MigrationService service;
+    @Autowired
+    private MigrationService service;
 
     @Autowired
-    public MigrationController(MigrationService service) {
-        this.service = service;
-    }
+    private CalcTask calcTask;
 
     @GetMapping("/mvn/lib/{libId}/get")
     public ResponseVO<List<MigrationGraphVO>> getMvnMigrationGraph(@PathVariable String libId) {
         return ResponseVO.success(this.service.relativeMigrationGraph(Integer.parseInt(libId)));
     }
 
+    @PostMapping("/mvn/calc/run")
+    public ResponseVO<Void> calcMigration() {
+        calcTask.process();
+        return ResponseVO.success(null);
+    }
 }
